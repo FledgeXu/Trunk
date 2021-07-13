a32a42c1e9fffd7581151ca4c24832990cb88af6
FledgeShiu <xzk0701@gmail.com>
2021-07-13 08:26:53 +0800

    Add ErrorTests.

---
+++ Tests/TrunkTests/ModelsTest/ErrorTests.swift
@@ -0,0 +1,27 @@
+//
+//  ErrorTests.swift
+//
+//
+//  Created by Fledge Shiu on 2021/7/12.
+//
+
+import XCTest
+import Foundation
+@testable import Trunk
+
+final class ErrorTests: XCTestCase {
+    let testJosn = """
+        {
+           "error":"invalid_grant",
+           "error_description":"The provided authorization grant is invalid, expired, revoked, does not match the redirection URI used in the authorization request, or was issued to another client."
+        }
+        """
+    let type = Error.self
+    func testExample() {
+        // results.
+        let jsonData = testJosn.data(using: .utf8)!
+        let result = try! JSONDecoder().decode(type, from: jsonData)
+        // Asserts
+        XCTAssertEqual(result.error, "invalid_grant")
+    }
+}
