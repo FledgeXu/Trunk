e8355118d04ce4ae33eb511e4f5dafa644edbe59
FledgeShiu <xzk0701@gmail.com>
2021-07-14 16:52:35 +0800

    Add AccountsVerifyCredentials

--- Sources/Trunk/Endpoints/AccountsEndpoint.swift
+++ Sources/Trunk/Endpoints/AccountsEndpoint.swift
@@ -32,6 +32,10 @@ public struct AccountsFiltersParameters: Encodable {
 }
 
 public enum AccountsEndpoint {
+    public static func verifyCredentials() -> Request<Account, EmptyEndpointParameters> {
+        return Request(path: "/api/v1/accounts/verify_credentials", method: .GET(.EMPTY))
+    }
+    
     public static func filters(parameters: AccountsFiltersParameters) -> Request<Filter, AccountsFiltersParameters> {
         return Request(path: "/api/v1/filters", method: .POST(.PARAMETERS(parameters)))
     }
