bf9564fc04f4be7e1f0ca6183868e15af9951a6b
FledgeShiu <xzk0701@gmail.com>
2021-07-16 17:11:17 +0800

    Add FollowRequestsEndpoint.

---
+++ Sources/Trunk/Endpoints/FollowRequestsEndpoint.swift
@@ -0,0 +1,16 @@
+//
+//  FollowRequestsEndpoint.swift
+//  
+//
+//  Created by Fledge Shiu on 2021/7/16.
+//
+
+import Foundation
+
+public enum FollowRequestsEndpoint {
+    public static func getPendingFollows(limit: Int? = nil) -> Request<[Account]> {
+        let toLimitBounds = between(1, and: 80, default: 40)
+        let parameters = [Parameter(key: "limit", value: limit.map(toLimitBounds).flatMap(toOptionalString))]
+        return Request(path: "/api/v1/follow_requests", method: .GET(.PARAMETERS(parameters)))
+    }
+}
