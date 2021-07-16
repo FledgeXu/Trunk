b1e0cbfeea2899d3e56216240a4095de2dda5983
FledgeShiu <xzk0701@gmail.com>
2021-07-16 13:00:24 +0800

    Add getBookmarkedStatuses.

--- Sources/Trunk/Endpoints/BookmarksEndpoint.swift
+++ Sources/Trunk/Endpoints/BookmarksEndpoint.swift
@@ -8,4 +8,17 @@
 import Foundation
 
 public enum BookmarksEndpoint {
+    public static func getBookmarkedStatuses(limit: Int? = nil,
+                                             maxId: String? = nil,
+                                             sinceId: String? = nil,
+                                             minId: String? = nil) -> Request<[Status]> {
+        let toLimitBounds = between(1, and: 80, default: 40)
+        let parameters = [
+            Parameter(key: "limit", value: limit.map(toLimitBounds).flatMap(toOptionalString)),
+            Parameter(key: "max_id", value: maxId),
+            Parameter(key: "since_id", value: sinceId),
+            Parameter(key: "min_id", value: minId),
+        ]
+        return Request(path: "/api/v1/bookmarks", method: .GET(.PARAMETERS(parameters)))
+    }
 }
