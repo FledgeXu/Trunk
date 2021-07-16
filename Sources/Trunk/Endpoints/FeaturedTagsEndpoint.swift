65b25cf1b98ff1c6549dfeb6f7be5b9190d94890
FledgeShiu <xzk0701@gmail.com>
2021-07-16 18:56:41 +0800

    Add unfeatureTag.

--- Sources/Trunk/Endpoints/FeaturedTagsEndpoint.swift
+++ Sources/Trunk/Endpoints/FeaturedTagsEndpoint.swift
@@ -13,7 +13,11 @@ public enum FeaturedTagsEndpoint {
     }
     
     public static func featureTag(name: String) -> Request<FeaturedTag> {
-        let parameters = [Parameter(key: "name", value: name),]
+        let parameters = [Parameter(key: "name", value: name)]
         return Request(path: "/api/v1/featured_tags", method: .POST(.PARAMETERS(parameters)))
+    }
+    
+    public static func unfeatureTag(id: Int) -> Request<Empty> {
+        return Request(path: "/api/v1/featured_tags/\(id)", method: .DELETE(.EMPTY))
     }
 }
