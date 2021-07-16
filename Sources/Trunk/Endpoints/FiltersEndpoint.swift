fb2aa47b6892bdd3c64c8d825f1c11c55568b466
FledgeShiu <xzk0701@gmail.com>
2021-07-16 16:46:35 +0800

    Add createFilter.

--- Sources/Trunk/Endpoints/FiltersEndpoint.swift
+++ Sources/Trunk/Endpoints/FiltersEndpoint.swift
@@ -15,4 +15,21 @@ public enum FiltersEndpoint {
     public static func getFilter(withId id: Int) -> Request<Filter> {
         return Request(path: "/api/v1/filters/\(id)", method: .GET(.EMPTY))
     }
+    
+    public static func createFilter(phrase: String,
+                                    context: [FilterContextType],
+                                    irreversible: Bool? = nil,
+                                    wholeWord: Bool? = nil,
+                                    expiresIn: Int? = nil) -> Request<Filter> {
+        var parameters = [
+            Parameter(key: "phrase", value: phrase),
+            Parameter(key: "irreversible", value: irreversible.flatMap(trueOrNil)),
+            Parameter(key: "whole_word", value: wholeWord.flatMap(trueOrNil)),
+            Parameter(key: "expires_in", value: expiresIn.flatMap(toOptionalString)),
+        ]
+        context.forEach({ type in
+            parameters.append(Parameter(key: "context[]", value: type.rawValue))
+        })
+        return Request(path: "/api/v1/filters", method: .POST(.PARAMETERS(parameters)))
+    }
 }
