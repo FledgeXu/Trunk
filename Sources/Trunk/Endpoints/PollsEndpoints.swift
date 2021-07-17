94aab50f93c56add5d632bd44138d163c5f3816c
FledgeShiu <xzk0701@gmail.com>
2021-07-17 19:56:45 +0800

    Add votePoll.

--- Sources/Trunk/Endpoints/PollsEndpoints.swift
+++ Sources/Trunk/Endpoints/PollsEndpoints.swift
@@ -11,4 +11,11 @@ public enum PollsEndpoints {
     public static func getPoll(id: String) -> Request<Poll> {
         return Request(path: "/api/v1/polls/\(id)", method: .GET(.EMPTY))
     }
+    
+    public static func votePoll(id: String, choices: [Int]) -> Request<Poll> {
+        let parameters = choices.map { choice in
+            return Parameter(key: "choices[]", value: String(choice))
+        }
+        return Request(path: "/api/v1/polls/\(id)/votes", method: .POST(.PARAMETERS(parameters)))
+    }
 }
