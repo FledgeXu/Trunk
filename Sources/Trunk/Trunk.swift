efe6adb8c8079e2223eb3142a1e7237dba3e2d7e
FledgeShiu <xzk0701@gmail.com>
2021-07-18 17:58:26 +0800

    Add comments to Trunk.

--- Sources/Trunk/Trunk.swift
+++ Sources/Trunk/Trunk.swift
@@ -5,16 +5,27 @@ public struct Trunk {
     let session: URLSession
     public var accessToken: String
     
+    /// Init Trunk Instance to run commands
+    /// - Parameters:
+    ///   - instanceURL: the URL of the Mastodon Instance
+    ///   - accessToken: App's accessToken
+    ///   - session: URLSession, default `URLSession.shared`
     public init(instanceURL: String, accessToken: String = "", session: URLSession = .shared) {
         self.instanceURL = instanceURL
         self.accessToken = accessToken
         self.session = session
     }
+    
+    /// Run Commands
+    /// - Parameters:
+    ///   - request: Endpoint API
+    ///   - completion: Hanlde the result.
     public func run<Model: Codable>(request: Request<Model>, completion: @escaping (_ result: Result<Model, TrunkError>, _ headers: [AnyHashable : Any]?) -> Void) {
         guard
             let components = URLComponents(baseURL: instanceURL, request: request),
             let url = components.url
         else {
+            // Make Sure URL is right.
             completion(.failure(.urlError), nil)
             return
         }
@@ -24,10 +35,12 @@ public struct Trunk {
                 completion(.failure(.dataTaskError(taskError)), nil)
                 return
             }
+            
             guard let data = data else {
                 completion(.failure(.malformedData), nil)
                 return
             }
+            
             guard
                 let httpResponse = response as? HTTPURLResponse
             else {
@@ -35,6 +48,7 @@ public struct Trunk {
                 completion(.failure(.responseErrorWithoutErrorType(str)), nil)
                 return
             }
+            
             if(200...299).contains(httpResponse.statusCode) {
                 do {
                     let result = try JSONDecoder().decode(Model.self, from: data)
@@ -51,6 +65,7 @@ public struct Trunk {
                 }
             }
         }
+        
         task.resume()
     }
 }
