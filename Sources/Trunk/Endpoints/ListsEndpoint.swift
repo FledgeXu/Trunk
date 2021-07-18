357d1b36cf5a1c475d59e7a2a846aecf94daf901
FledgeShiu <xzk0701@gmail.com>
2021-07-18 09:42:30 +0800

    Add getLists.

---
+++ Sources/Trunk/Endpoints/ListsEndpoint.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Fledge Shiu on 2021/7/18.
+//
+
+import Foundation
+
+public enum ListsEndpoint {
+    public static func getLists() -> Request<[List]> {
+        return Request(path: "/api/v1/lists")
+    }
+}
