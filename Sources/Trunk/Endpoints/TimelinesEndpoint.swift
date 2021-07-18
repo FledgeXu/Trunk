ba7700d818148dfa795f608210b55b5890cd407b
FledgeShiu <xzk0701@gmail.com>
2021-07-18 16:33:47 +0800

    Add comments to TimelinesEndpoint.

--- Sources/Trunk/Endpoints/TimelinesEndpoint.swift
+++ Sources/Trunk/Endpoints/TimelinesEndpoint.swift
@@ -1,5 +1,5 @@
 //
-//  Timelines.swift
+//  TimelinesEndpoint.swift
 //  
 //
 //  Created by Fledge Shiu on 2021/4/23.
@@ -8,7 +8,19 @@
 import Foundation
 
 public enum TimelinesEndpoint {
-    public static func `public`(local: Bool? = nil,
+    
+    /// Get public timeline
+    /// Endpoint: `/api/v1/timelines/public`
+    /// - Parameters:
+    ///   - local: Show only local statuses? Defaults to false.
+    ///   - remote: Show only remote statuses? Defaults to false.
+    ///   - onlyMedia: Show only statuses with media attached? Defaults to false.
+    ///   - maxID: Return results older than this id
+    ///   - sinceID: Return results newer than this id
+    ///   - minID: Return results immediately newer than this id
+    ///   - limit: Maximum number of results to return. Defaults to 20.
+    /// - Returns: Array of Status
+    public static func publicTimeline(local: Bool? = nil,
                                 remote: Bool? = nil,
                                 onlyMedia: Bool? = nil,
                                 maxID: String? = nil,
@@ -28,7 +40,18 @@ public enum TimelinesEndpoint {
         return Request(path: "/api/v1/timelines/public", method: .GET(.PARAMETERS(parameters)))
     }
     
-    public static func getHashtagTimeline(hashtag: String,
+    /// View public statuses containing the given hashtag.
+    /// Endpoint: `/api/v1/timelines/tag/:hashtag`
+    /// - Parameters:
+    ///   - hashtag: Content of a #hashtag, not including # symbol.
+    ///   - local: If true, return only local statuses. Defaults to false.
+    ///   - onlyMedia: If true, return only statuses with media attachments. Defaults to false.
+    ///   - maxID: Return results older than this ID.
+    ///   - sinceID: Return results newer than this ID.
+    ///   - minID: Return results immediately newer than this ID.
+    ///   - limit: Maximum number of results to return. Defaults to 20.
+    /// - Returns: Array of Status
+    public static func hashtagTimeline(hashtag: String,
                                           local: Bool? = nil,
                                           onlyMedia: Bool? = nil,
                                           maxID: String? = nil,
@@ -47,6 +70,15 @@ public enum TimelinesEndpoint {
         return Request(path: "/api/v1/timelines/tag/\(hashtag)", method: .GET(.PARAMETERS(parameters)))
     }
     
+    /// View statuses from followed users.
+    /// Endpoint: `/api/v1/timelines/home`
+    /// - Parameters:
+    ///   - local: Return only local statuses?
+    ///   - maxID: Return results older than id
+    ///   - sinceID: Return results newer than id
+    ///   - minID: Return results immediately newer than id
+    ///   - limit: Maximum number of results to return. Defaults to 20.
+    /// - Returns: Array of Status
     public static func homeTimeline(local: Bool? = nil,
                                     maxID: String? = nil,
                                     sinceID: String? = nil,
@@ -63,6 +95,15 @@ public enum TimelinesEndpoint {
         return Request(path: "/api/v1/timelines/home", method: .GET(.PARAMETERS(parameters)))
     }
     
+    /// View statuses in the given list timeline.
+    /// Endpoint: `/api/v1/timelines/list/:list_id`
+    /// - Parameters:
+    ///   - id: Local ID of the list in the database.
+    ///   - maxID: Return results older than this ID.
+    ///   - sinceID: Return results newer than this ID.
+    ///   - minID: Return results immediately newer than this ID.
+    ///   - limit: Maximum number of results to return. Defaults to 20.Return results older than this ID.
+    /// - Returns: Array of Status
     public static func listTimeline(id: String,
                                     maxID: String? = nil,
                                     sinceID: String? = nil,
