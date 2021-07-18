f3170bca83611c1f4de47890f134e39c01c85f7b
FledgeShiu <xzk0701@gmail.com>
2021-07-18 16:46:23 +0800

    Add comments to ListsEndpoint.

--- Sources/Trunk/Endpoints/ListsEndpoint.swift
+++ Sources/Trunk/Endpoints/ListsEndpoint.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  ListsEndpoint.swift
 //  
 //
 //  Created by Fledge Shiu on 2021/7/18.
@@ -8,14 +8,29 @@
 import Foundation
 
 public enum ListsEndpoint {
-    public static func getLists() -> Request<[List]> {
+    
+    /// Fetch all lists that the user owns.
+    /// Endpoint: `/api/v1/lists`
+    /// - Returns: Array of List
+    public static func showUserLists() -> Request<[List]> {
         return Request(path: "/api/v1/lists")
     }
     
-    public static func getSingleLists(id: String) -> Request<List> {
+    
+    /// Fetch the list with the given ID. Used for verifying the title of a list, and which replies to show within that list.
+    /// Endpoint: `/api/v1/lists/:id`
+    /// - Parameter id: ID of the list in the database
+    /// - Returns: List
+    public static func showSingleLists(id: String) -> Request<List> {
         return Request(path: "/api/v1/lists/\(id)")
     }
     
+    /// Create a new list.
+    /// Endpoint: `/api/v1/lists`
+    /// - Parameters:
+    ///   - title: The title of the list to be created.
+    ///   - repliesPolicy: Enumerable oneOf followed list none. Defaults to list.
+    /// - Returns: List
     public static func createList(title: String, repliesPolicy: RepliesPolicyType? = nil) -> Request<List> {
         let parameters = [
             Parameter(key: "title", value: title),
@@ -24,6 +39,13 @@ public enum ListsEndpoint {
         return Request(path: "/api/v1/lists", method: .POST(.PARAMETERS(parameters)))
     }
     
+    /// Change the title of a list, or which replies to show.
+    /// Endpoint: `/api/v1/lists/:id`
+    /// - Parameters:
+    ///   - id: ID of the list in the database
+    ///   - title: The title of the list to be updated.
+    ///   - repliesPolicy: Enumerable oneOf followed list none.
+    /// - Returns: List
     public static func updateList(id: String,
                                   title: String,
                                   repliesPolicy: RepliesPolicyType? = nil) -> Request<List> {
@@ -34,14 +56,37 @@ public enum ListsEndpoint {
         return Request(path: "/api/v1/lists/\(id)", method: .PUT(.PARAMETERS(parameters)))
     }
     
+    /// Delete a list
+    /// Endpoint: `/api/v1/lists/:id`
+    /// - Parameter id: ID of the list in the database
+    /// - Returns: Empty
     public static func deleteList(id: String) -> Request<Empty> {
         return Request(path: "/api/v1/lists/\(id)", method: .DELETE(.EMPTY))
     }
     
-    public static func getAccountsInList(id: String) -> Request<[Account]> {
-        return Request(path: "/api/v1/lists/\(id)/accounts", method: .GET(.EMPTY))
+    /// View accounts in list
+    /// Endpoint: `/api/v1/lists/:id/accounts`
+    /// - Parameter id: ID of the list in the database
+    /// - Returns: Array of Account
+    public static func viewAccountsInList(id: String,
+                                          limit: Int? = nil,
+                                          maxId: String? = nil,
+                                          sinceId: String? = nil) -> Request<[Account]> {
+        let toBetween = between(1, and: 80, default: 40)
+        let parameters = [
+            Parameter(key: "max_id", value: maxId),
+            Parameter(key: "since_id", value: sinceId),
+            Parameter(key: "limit", value: limit.map(toBetween).flatMap(toOptionalString))
+        ]
+        return Request(path: "/api/v1/lists/\(id)/accounts", method: .GET(.PARAMETERS(parameters)))
     }
     
+    /// Add accounts to the given list. Note that the user must be following these accounts.
+    /// Endpoint: `/api/v1/lists/:id/accounts`
+    /// - Parameters:
+    ///   - id: ID of the list in the database
+    ///   - accountIds: Array of account IDs to add to the list.
+    /// - Returns: Empty
     public static func addAccountsToList(id: String, accountIds: [String]) -> Request<Empty> {
         let parameters = accountIds.map { id in
             Parameter(key: "account_ids[]", value: id)
@@ -49,6 +94,12 @@ public enum ListsEndpoint {
         return Request(path: "/api/v1/lists/\(id)/accounts", method: .POST(.PARAMETERS(parameters)))
     }
     
+    /// Remove accounts from the given list.
+    /// Endpoint: `/api/v1/lists/:id/accounts`
+    /// - Parameters:
+    ///   - id: ID of the list in the database
+    ///   - accountIds: Array of account IDs to remove from the list.
+    /// - Returns: Empty
     public static func removeAccountsFromList(id: String, accountIds: [String]) -> Request<Empty> {
         let parameters = accountIds.map { id in
             Parameter(key: "account_ids[]", value: id)
