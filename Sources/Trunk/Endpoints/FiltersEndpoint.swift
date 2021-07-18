d204be748f3e3a1d3fd4df9fe5f84c4276ab2274
FledgeShiu <xzk0701@gmail.com>
2021-07-18 13:55:15 +0800

    Add comments to FiltersEndpoint.

--- Sources/Trunk/Endpoints/FiltersEndpoint.swift
+++ Sources/Trunk/Endpoints/FiltersEndpoint.swift
@@ -8,14 +8,33 @@
 import Foundation
 
 public enum FiltersEndpoint {
-    public static func getFilters() -> Request<[Filter]> {
+    
+    /// View all filters
+    /// Endpoint: `/api/v1/filters`
+    /// - Returns: Filter
+    public static func viewAllFilters() -> Request<[Filter]> {
         return Request(path: "/api/v1/filters", method: .GET(.EMPTY))
     }
     
-    public static func getFilter(withId id: String) -> Request<Filter> {
+    
+    /// View a single filter
+    /// Endpoint: `/api/v1/filters/:id`
+    /// - Parameter id: Filter id
+    /// - Returns: Filter
+    public static func viewSingleFilter(id: String) -> Request<Filter> {
         return Request(path: "/api/v1/filters/\(id)", method: .GET(.EMPTY))
     }
     
+    
+    /// Create a Filter
+    /// Endpoint: `/api/v1/filters`
+    /// - Parameters:
+    ///   - phrase: Text to be filtered
+    ///   - context: Array of enumerable strings `home`, `notifications`, `public`, `thread`. At least one context must be specified.
+    ///   - irreversible: Should the server irreversibly drop matching entities from home and notifications?
+    ///   - wholeWord: Consider word boundaries?
+    ///   - expiresIn: Number of seconds from now the filter should expire. Otherwise, null for a filter that doesn't expire.
+    /// - Returns: Filter
     public static func createFilter(phrase: String,
                                     context: [FilterContextType],
                                     irreversible: Bool? = nil,
@@ -33,6 +52,16 @@ public enum FiltersEndpoint {
         return Request(path: "/api/v1/filters", method: .POST(.PARAMETERS(parameters)))
     }
     
+    /// Update a Filter
+    /// Endpoint: `/api/v1/filters/:id`
+    /// - Parameters:
+    ///   - id: ID of the filter in the database
+    ///   - phrase: Text to be filtered
+    ///   - context: Array of enumerable strings `home`, `notifications`, `public`, `thread`. At least one context must be specified.
+    ///   - irreversible: Should the server irreversibly drop matching entities from home and notifications?
+    ///   - wholeWord: Consider word boundaries?
+    ///   - expiresIn: Number of seconds from now the filter should expire. Otherwise, null for a filter that doesn't expire.
+    /// - Returns: Filter
     public static func updateFilter(id: String,
                                     phrase: String,
                                     context: [FilterContextType],
@@ -51,6 +80,10 @@ public enum FiltersEndpoint {
         return Request(path: "/api/v1/filters/\(id)", method: .PUT(.PARAMETERS(parameters)))
     }
     
+    /// Remove a filter
+    /// Endpoint: `/api/v1/filters/:id`
+    /// - Parameter id: ID of the filter in the database
+    /// - Returns: Filter
     public static func removeFilter(id: String) -> Request<Empty> {
         return Request(path: "/api/v1/filters/\(id)", method: .DELETE(.EMPTY))
     }
