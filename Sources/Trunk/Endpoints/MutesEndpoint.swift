c6488c1c71526be8e62418758cf75d629f12a8e4
FledgeShiu <xzk0701@gmail.com>
2021-07-18 13:45:05 +0800

    Add comments to MutesEndpoint.

--- Sources/Trunk/Endpoints/MutesEndpoint.swift
+++ Sources/Trunk/Endpoints/MutesEndpoint.swift
@@ -7,7 +7,15 @@
 
 import Foundation
 public enum MutesEndpoint {
-    public static func getMutedAccounts(limit: Int? = nil,
+    
+    /// Accounts the user has muted.
+    /// Endpoint: `/api/v1/mutes`
+    /// - Parameters:
+    ///   - limit: Maximum number of results to return per page. Defaults to 40.
+    ///   - maxId: Internal parameter. Use the HTTP Link header for pagination instead.
+    ///   - sinceId: Internal parameter. Use the HTTP Link header for pagination instead.
+    /// - Returns: Array of Account
+    public static func mutedAccounts(limit: Int? = nil,
                                              maxId: String? = nil,
                                              sinceId: String? = nil) -> Request<[Account]> {
         let toLimitBounds = between(1, and: 80, default: 40)
