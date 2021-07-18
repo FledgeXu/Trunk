16cb2b3e18fe5f0841e192d9961a1545c15eb675
FledgeShiu <xzk0701@gmail.com>
2021-07-18 16:22:15 +0800

    Add comments to MediaEndpoints.

--- Sources/Trunk/Endpoints/MediaEndpoints.swift
+++ Sources/Trunk/Endpoints/MediaEndpoints.swift
@@ -8,7 +8,16 @@
 import Foundation
 
 public enum MediaEndpoints {
-    public static func uploadAttachment(file: Data,
+    
+    /// Creates an attachment to be used with a new status.
+    /// Endpoint: `/api/v1/media`
+    /// - Parameters:
+    ///   - file: The file to be attached, using multipart form data.
+    ///   - thumbnail: The custom thumbnail of the media to be attached, using multipart form data.
+    ///   - description: A plain-text description of the media, for accessibility purposes.
+    ///   - focus: Two floating points (x,y), comma-delimited, ranging from -1.0 to 1.0
+    /// - Returns: Attachment
+    public static func uploadMediaAsAttachment(file: Data,
                                         thumbnail: Data? = nil,
                                         description: String? = nil,
                                         focus: String? = nil) -> Request<Attachment> {
@@ -23,10 +32,23 @@ public enum MediaEndpoints {
         return Request(path: "/api/v2/media", method: .POST(.MEDIA(parameters, dataParameters)))
     }
     
+    /// Get an Attachment, before it is attached to a status and posted, but after it is accepted for processing.
+    /// Endpoint: `/api/v1/media`
+    /// - Parameter id: The id of the Attachment entity to be updated
+    /// - Returns: Attachment
     public static func getAttachment(id: String) -> Request<Attachment> {
         return Request(path: "/api/v1/media/\(id)", method: .GET(.EMPTY))
     }
     
+    /// Update an Attachment, before it is attached to a status and posted.
+    /// Endpoint: `/api/v1/media/:id`
+    /// - Parameters:
+    ///   - id: The id of the Attachment entity to be updated
+    ///   - file: The file to be attached, using multipart form data.
+    ///   - thumbnail: The custom thumbnail of the media to be attached, using multipart form data.
+    ///   - description: A plain-text description of the media, for accessibility purposes.
+    ///   - focus: Two floating points (x,y), comma-delimited ranging from -1.0 to 1.0
+    /// - Returns: Attachment
     public static func updateAttachment(id: String,
                                         file: Data,
                                         thumbnail: Data? = nil,
