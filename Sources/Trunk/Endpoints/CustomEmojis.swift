be2fa192965e7db6041af7a1e1c7c74d2dd88592
FledgeShiu <xzk0701@gmail.com>
2021-07-18 17:01:32 +0800

    Add comments to CustomEmojis.

--- Sources/Trunk/Endpoints/CustomEmojis.swift
+++ Sources/Trunk/Endpoints/CustomEmojis.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  CustomEmojis.swift
 //  
 //
 //  Created by Fledge Shiu on 2021/7/18.
@@ -8,7 +8,11 @@
 import Foundation
 
 public enum CustomEmojis {
-    public static func getCustomEmojis() -> Request<[Emoji]> {
+    
+    /// Returns custom emojis that are available on the server.
+    /// Endpoint: `/api/v1/custom_emojis`
+    /// - Returns: Array of Emoji
+    public static func customEmojis() -> Request<[Emoji]> {
         return Request(path: "/api/v1/custom_emojis")
     }
 }
