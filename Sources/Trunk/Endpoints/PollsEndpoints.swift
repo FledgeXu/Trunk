2e0bc20427ea343bdccda9dc357d86108942c23e
FledgeShiu <xzk0701@gmail.com>
2021-07-18 16:24:10 +0800

    Add comments to PollsEndpoints.

--- Sources/Trunk/Endpoints/PollsEndpoints.swift
+++ Sources/Trunk/Endpoints/PollsEndpoints.swift
@@ -8,11 +8,22 @@
 import Foundation
 
 public enum PollsEndpoints {
-    public static func getPoll(id: String) -> Request<Poll> {
+    
+    /// View a poll
+    /// Endpoint: `/api/v1/polls/:id`
+    /// - Parameter id: ID of the poll in the database
+    /// - Returns: Poll
+    public static func viewPoll(id: String) -> Request<Poll> {
         return Request(path: "/api/v1/polls/\(id)", method: .GET(.EMPTY))
     }
     
-    public static func votePoll(id: String, choices: [Int]) -> Request<Poll> {
+    /// Vote on a poll
+    /// Endpoint: `/api/v1/polls/:id/votes`
+    /// - Parameters:
+    ///   - id: ID of the poll in the database
+    ///   - choices: Array of own votes containing index for each option (starting from 0)
+    /// - Returns: Poll
+    public static func voteOnPoll(id: String, choices: [Int]) -> Request<Poll> {
         let parameters = choices.map { choice in
             return Parameter(key: "choices[]", value: String(choice))
         }
