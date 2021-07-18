b9320e49c42812ef38ae712fc4500848f41d3ddc
FledgeShiu <xzk0701@gmail.com>
2021-07-18 13:58:29 +0800

    Add comments to FollowRequestsEndpoint.

--- Sources/Trunk/Endpoints/FollowRequestsEndpoint.swift
+++ Sources/Trunk/Endpoints/FollowRequestsEndpoint.swift
@@ -8,16 +8,27 @@
 import Foundation
 
 public enum FollowRequestsEndpoint {
-    public static func getPendingFollows(limit: Int? = nil) -> Request<[Account]> {
+    
+    /// Get pending follows
+    /// Endpoint: `/api/v1/follow_requests`
+    /// - Parameter limit: Maximum number of results to return. Defaults to 40. Paginate using the HTTP Link header.
+    /// - Returns: Array of Account
+    public static func pendingFollows(limit: Int? = nil) -> Request<[Account]> {
         let toLimitBounds = between(1, and: 80, default: 40)
         let parameters = [Parameter(key: "limit", value: limit.map(toLimitBounds).flatMap(toOptionalString))]
         return Request(path: "/api/v1/follow_requests", method: .GET(.PARAMETERS(parameters)))
     }
     
+    /// Accept follow
+    /// - Parameter id: ID of the account in the database
+    /// - Returns: Relationship
     public static func acceptFollow(id: String) -> Request<Relationship> {
         return Request(path: "/api/v1/follow_requests/\(id)/authorize", method: .POST(.EMPTY))
     }
     
+    /// Reject follow
+    /// - Parameter id: ID of the account in the database
+    /// - Returns: Relationship
     public static func rejectFollow(id: String) -> Request<Relationship> {
         return Request(path: "/api/v1/follow_requests/\(id)/reject", method: .POST(.EMPTY))
     }
