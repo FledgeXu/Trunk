8a8c82df96c5a673e81446bfd5d6a3162049151a
FledgeShiu <xzk0701@gmail.com>
2021-07-18 13:50:55 +0800

    Add comments to DomainBlocksEndpoint.

--- Sources/Trunk/Endpoints/DomainBlocksEndpoint.swift
+++ Sources/Trunk/Endpoints/DomainBlocksEndpoint.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  DomainBlocksEndpoint.swift
 //  
 //
 //  Created by Fledge Shiu on 2021/7/16.
@@ -8,7 +8,15 @@
 import Foundation
 
 public enum DomainBlocksEndpoint {
-    public static func getDomainBlocks(limit: Int? = nil,
+    
+    /// View domains the user has blocked.
+    /// Endpoint: `/api/v1/domain_blocks`
+    /// - Parameters:
+    ///   - limit: Maximum number of results to return per page. Defaults to 40. NOTE: Pagination is done with the Link header from the response.
+    ///   - maxId: Internal parameter. Use HTTP Link header from response for pagination.
+    ///   - sinceId: Internal parameter. Use HTTP Link header from response for pagination.
+    /// - Returns: Array of strings
+    public static func fetchDomainBlocks(limit: Int? = nil,
                                              maxId: String? = nil,
                                              sinceId: String? = nil) -> Request<[String]> {
         let toLimitBounds = between(1, and: 80, default: 40)
@@ -20,11 +28,23 @@ public enum DomainBlocksEndpoint {
         return Request(path: "/api/v1/domain_blocks", method: .GET(.PARAMETERS(parameters)))
     }
     
+    /// Block a domain to:
+    /// hide all public posts from it
+    /// hide all notifications from it
+    /// remove all followers from it
+    /// prevent following new users from it (but does not remove existing follows)
+    /// Endpoint: `/api/v1/domain_blocks`
+    /// - Parameter domain: Domain to block
+    /// - Returns: Empty
     public static func blockDomain(domain: String) -> Request<Empty> {
         let parameters = [Parameter(key: "domain",value: domain)]
         return Request(path: "/api/v1/domain_blocks", method: .POST(.PARAMETERS(parameters)))
     }
     
+    /// Remove a domain block, if it exists in the user's array of blocked domains.
+    /// Endpoint: `/api/v1/domain_blocks`
+    /// - Parameter domain: Domain to unblock.
+    /// - Returns: Empty
     public static func unblockDomain(domain: String) -> Request<Empty> {
         let parameters = [Parameter(key: "domain",value: domain)]
         return Request(path: "/api/v1/domain_blocks", method: .DELETE(.PARAMETERS(parameters)))
