e291ac1103ba86d881ce38eb6b6702357fe856ad
FledgeShiu <xzk0701@gmail.com>
2021-07-18 13:06:42 +0800

    Add FavouritesEndpoint.

--- Sources/Trunk/Endpoints/FavouritesEndpoint.swift
+++ Sources/Trunk/Endpoints/FavouritesEndpoint.swift
@@ -8,9 +8,17 @@
 import Foundation
 
 public enum FavouritesEndpoint {
-    public static func getFavouritedStatuses(limit: Int? = nil,
-                                             maxId: String? = nil,
-                                             minId: String? = nil) -> Request<[Status]> {
+    
+    /// Statuses the user has favourited.
+    /// Endpoint: `/api/v1/favourites`
+    /// - Parameters:
+    ///   - limit:
+    ///   - maxId: Internal parameter. Use HTTP Link header for pagination instead.
+    ///   - minId: Internal parameter. Use HTTP Link header for pagination instead.
+    /// - Returns: Array of Status
+    public static func favouritedStatuses(limit: Int? = nil,
+                                          maxId: String? = nil,
+                                          minId: String? = nil) -> Request<[Status]> {
         let toLimitBounds = between(1, and: 80, default: 40)
         let parameters = [
             Parameter(key: "limit", value: limit.map(toLimitBounds).flatMap(toOptionalString)),
