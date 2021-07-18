ad36bcb779ff87b4821d30ef5e3bdfc230e23521
FledgeShiu <xzk0701@gmail.com>
2021-07-18 12:10:37 +0800

    Comments AppsEndpoint.

--- Sources/Trunk/Endpoints/AppsEndpoint.swift
+++ Sources/Trunk/Endpoints/AppsEndpoint.swift
@@ -8,6 +8,15 @@
 import Foundation
 
 public enum AppsEndpoint {
+    
+    /// Create a new application to obtain OAuth2 credentials.
+    /// Endpoint: `/api/v1/apps`
+    /// - Parameters:
+    ///   - clientName: A name for your application
+    ///   - redirectUris: Where the user should be redirected after authorization. To display the authorization code to the user instead of redirecting to a web page, use `urn:ietf:wg:oauth:2.0:oob` in this parameter.
+    ///   - scopes: Space separated list of scopes. If none is provided, defaults to `read`.
+    ///   - website: A URL to the homepage of your app
+    /// - Returns: Application, with `client_id` and `client_secret`
     public static func createApplication(clientName: String,
                                          redirectUris: String = "urn:ietf:wg:oauth:2.0:oob",
                                          scopes: [OAuthScopes]? = nil,
@@ -21,7 +30,10 @@ public enum AppsEndpoint {
         return Request(path: "/api/v1/apps", method: .POST(.PARAMETERS(parameters)))
     }
     
-    public static func verifyCredentials() -> Request<Application> {
+    
+    /// Confirm that the app's OAuth2 credentials work.
+    /// - Returns: Application
+    public static func verifyAppWorks() -> Request<Application> {
         return Request(path: "/api/v1/apps/verify_credentials", method: .GET(.EMPTY))
     }
 }
