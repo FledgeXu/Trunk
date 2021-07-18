c69140b400e40bffb2395c3a034c94c55121d387
FledgeShiu <xzk0701@gmail.com>
2021-07-18 16:59:03 +0800

    Add comments to trendingTags.

--- Sources/Trunk/Endpoints/TrendsEndpoint.swift
+++ Sources/Trunk/Endpoints/TrendsEndpoint.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  TrendsEndpoint.swift
 //  
 //
 //  Created by Fledge Shiu on 2021/7/18.
@@ -8,7 +8,12 @@
 import Foundation
 
 public enum TrendsEndpoint {
-    public static func getTrendingTags(limit: Int? = nil) -> Request<[Tag]> {
+    
+    /// Tags that are being used more frequently within the past week.
+    /// Endpoint: `/api/v1/trends`
+    /// - Parameter limit: Maximum number of results to return. Defaults to 10.
+    /// - Returns: Array of Tag with History
+    public static func trendingTags(limit: Int? = nil) -> Request<[Tag]> {
         let toBetween = between(1, and: 20, default: 10)
         let parameters = [Parameter(key: "limit", value: limit.map(toBetween).flatMap(toOptionalString))]
         return Request(path: "/api/v1/trends", method: .GET(.PARAMETERS(parameters)))
